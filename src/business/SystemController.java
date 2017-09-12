1bc9a04796cd02fddd6e494fc0c327c8c2d62899
Thanh Huy <985668@cs.mum.edu>
2017-09-12 14:06:00 -0500

    Update code for checking out book.

--- src/business/SystemController.java
+++ src/business/SystemController.java
@@ -9,10 +9,25 @@ import dataaccess.Auth;
 import dataaccess.DataAccess;
 import dataaccess.DataAccessFacade;
 import dataaccess.User;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.text.Text;
+import javafx.fxml.FXML;
+import ui.Start;
 
 public class SystemController implements ControllerInterface {
 	public static Auth currentAuth = null;
-	
+    @FXML private Button checkBtn;
+    @FXML private Text statusCheckout;
+    @FXML private TableColumn memberCol;
+    @FXML private TableColumn isbnCol;
+    @FXML private TableColumn checkoutDateCol;
+    @FXML private TableColumn dueDateCol;
+
+	@Override
 	public void login(String id, String password) throws LoginException {
 		DataAccess da = new DataAccessFacade();
 		HashMap<String, User> map = da.readUserMap();
@@ -24,8 +39,8 @@ public class SystemController implements ControllerInterface {
 			throw new LoginException("Password incorrect");
 		}
 		currentAuth = map.get(id).getAuthorization();
-		
 	}
+
 	@Override
 	public List<String> allMemberIds() {
 		DataAccess da = new DataAccessFacade();
@@ -33,7 +48,7 @@ public class SystemController implements ControllerInterface {
 		retval.addAll(da.readMemberMap().keySet());
 		return retval;
 	}
-	
+
 	@Override
 	public List<String> allBookIds() {
 		DataAccess da = new DataAccessFacade();
@@ -41,6 +56,23 @@ public class SystemController implements ControllerInterface {
 		retval.addAll(da.readBooksMap().keySet());
 		return retval;
 	}
-	
-	
+
+	@Override
+	public void checkAvailable(String id, String isbn) {
+		DataAccess da = new DataAccessFacade();
+		List<String> retval = new ArrayList<>();
+		retval.addAll(da.readMemberMap().keySet());
+		if (!retval.contains(id))
+			statusCheckout.setText("Member ID is not found");
+		retval.addAll(da.readBooksMap().keySet());
+		if (!retval.contains(isbn))
+			statusCheckout.setText("The book is not available");
+		Book book = da.readBooksMap().get(isbn);
+		if (!book.isAvailable())
+			statusCheckout.setText("The book is not available");
+		else statusCheckout.setText("The book is available");
+
+
+	}
+
 }
